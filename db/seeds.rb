785bd95e007a2de316d7ece200d32b5fea77a254
Corrie <corrieleech@gmail.com>
2022-02-10 20:28:53 -0600

    adds backend api for places front end app, with restful routes

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# place = Place.create(name: "Home", address: "123 Fake Street, Chicago, IL 60600")
+# place = Place.create(name: "Grandmas", address: "225 Comanche Street, Waukesha, WI 53100")
+# place = Place.create(name: "The Cabin", address: "123 Sugarview Lane, Green Lake, WI 53500")
+# place = Place.create(name: "Amanda and Wonjun's", address: "123 Hilly Lane, Portland, OR 97000")
+# place = Place.create(name: "Box Car Hole, Links Course", address: "123 Wild Turkey Lane, Green Lake, WI 53500")
